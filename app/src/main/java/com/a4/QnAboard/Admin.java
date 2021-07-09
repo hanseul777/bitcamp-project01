7b57083a2dd4d266d5d870f55683f2908a01e847
yong025 <yong025@naver.com>
2021-07-09 14:23:02 +0900

    push

---
+++ app/src/main/java/com/a4/QnAboard/Admin.java
@@ -0,0 +1,32 @@
+package com.a4.QnAboard;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Scanner;
+
+public class Admin {
+
+  Connection CN=null; //DB서버연결정보 서버ip주소 계정id,pwd
+  Statement ST=null;  //ST=CN.createStatement()명령어생성 삭제,신규등록,조회하라
+  ResultSet RS=null;  //select조회결과값 전체데이터를 기억합니다
+  String msg="";
+  int Gtotal = 0;  
+  Scanner sc = new Scanner(System.in);
+
+  public static void main(String[] args) {
+    Admin ad = new Admin();
+    ad.DBbase();
+  }
+
+  public void DBbase() {
+    try {
+      Class.forName("oracle.jdbc.driver.OracleDriver"); 
+      CN=DriverManager.getConnection("jdbc:oracle:thin:@127.0.0.1:1521:xe","system","1234");
+      System.out.println("오라클 드라이브및 서버연결성공 ");    
+      ST = CN.createStatement(); 
+    }catch(Exception e){System.out.println("error =" + e);}
+  }//end
+}
+
