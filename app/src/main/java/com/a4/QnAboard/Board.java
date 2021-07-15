0bb0029743c914e1d944feac083ee7bdae15fe63
ljw8124 <ljw8124@gmail.com>
2021-07-15 10:19:10 +0900

    project

--- app/src/main/java/com/a4/QnAboard/Board.java
+++ app/src/main/java/com/a4/QnAboard/Board.java
@@ -12,31 +12,31 @@ public class Board extends DB{
     DB db = new DB();
     db.DBbase();
     System.out.println("\"-----------------------------------[자유 게시판]---------------------------------------");
-    boardHandler.boardBoard("freeboard");
+    boardHandler.viewBoard("freeboard");
     System.out.println("-----------------------------------------------------------------------------------------");
     System.out.println("-----------------------------------[질문답변 게시판]-------------------------------------");
-    boardHandler.boardBoard("board");
+    boardHandler.viewBoard("board");
     System.out.println("-----------------------------------------------------------------------------------------");
     System.out.println("-----------------------------------[수업진도 게시판]-------------------------------------");
-    boardHandler.boardBoard("review");
+    boardHandler.viewBoard("review");
     System.out.println("-----------------------------------------------------------------------------------------");
-    System.out.println();
     System.out.println("----------------------------------[스터디모집 게시판]------------------------------------");
     appointment.viewAppointment();
-    System.out.println();
     System.out.println("-----------------------------------------------------------------------------------------");
     System.out.println();
 
     System.out.println("[게시판 목록] : freeBoard/ board/ review/ appointment");
-    System.out.println("입장할 게시판을 입력해주세요 : ");
+    System.out.println("[단축어] : [f]-freeBoard [b]-board [r]-review [a]-appointment");
+    System.out.print("입장할 게시판을 입력해주세요 : ");
     String input = keyScan.nextLine();
 
     try {
-      if (input.equals("appointment")) {
+      if (input.equals("a")) {
         appointment.viewAppointment();
         loop: while (true) {
           System.out.println("1.게시판 조회");
           System.out.println("2.게시글 작성");
+          System.out.println("3.게시글 수정");
           System.out.println("4.게시글 삭제");
           System.out.println("6. 뒤로 가기");
           System.out.print("입력 : ");
@@ -45,9 +45,9 @@ public class Board extends DB{
           switch (select) {
             case 1: appointment.viewAppointment(); break;
             case 2: appointment.insertAppointment(); break;
-            //case 3: boardHandler.updateBoard(input); break;
-            case 4: boardHandler.deleteBoard(input); break;
-            //case 5: boardHandler.search(input); break;
+            //case 3: boardHandler.updateBoard(); break;
+            case 4: appointment.deleteBoard(); break;
+            //case 5: boardHandler.search(); break;
             case 6: break loop;
           }
         }
@@ -64,7 +64,7 @@ public class Board extends DB{
           int select  = keyScan.nextInt();
 
           switch (select) {
-            case 1: boardHandler.viewBoard(input); break;
+            case 1: boardHandler.viewReply(input); break;
             case 2: boardHandler.insertBoard(input); break;
             case 3: boardHandler.updateBoard(input); break;
             case 4: boardHandler.deleteBoard(input); break;
